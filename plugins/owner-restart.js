b5e46d96a07ff653405e5b8ebf626967f94f3588
mysto off <khadabfest@gmail.com>
2026-08-26 11:11:42 +0100

    Update owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -1,16 +1,47 @@
 import { parentPort } from 'worker_threads';
 
+// ===== Channel Info SASUKE =====
+const channelName = 'ᏚᎯᏚᏌᏦᎬ ᎿᎬᏨᎻ 🇲🇦'
+const CHANNEL_ID = '120363427685476208@newsletter'
+const newsletter = {
+  forwardingScore: 999,
+  isForwarded: true,
+  forwardedNewsletterMessageInfo: {
+    newsletterJid: CHANNEL_ID,
+    newsletterName: channelName
+  }
+}
+// ========================
+
 let handler = async (m, { conn }) => {
-	if (!parentPort) throw 'Dont: node main.js\nDo: node index.js';
+	
+	const sendWithChannel = async (txt) => {
+        await conn.sendMessage(m.chat, { 
+            text: txt,
+            contextInfo: newsletter
+        }, { quoted: m })
+    }
+
+	if (!parentPort) throw '*❌ خـطـأ:* شـغـل الـبـوت بـ `node index.js` مـاشـي `node main.js`'
+	
 	if (global.conn.user.jid == conn.user.jid) {
-		await m.reply('```R E S T A R T . . .```');
+		await m.react('🔄')
+		await sendWithChannel(`
+*🔄 SASUKE RESTART*
+
+*جـاري اعـادة الـتـشـغـيـل ✨*
+
+*⚡ الـحـالـة:* \`SASUKE TECH . .\`
+*⏳ يتـم إعــادة تشـغـيــل مـن جـديــد*
+
+*© SASUKE TECH*`)
+		
 		parentPort.postMessage('restart');
-	} else throw '_eeeeeiiittsssss..._';
+	} else throw '*⚠️ مـمـنـوع:* هـاد الامـر غـيـر لـلـمـالـك';
 };
 
-handler.help = ['restart'];
-handler.tags = ['owner'];
-handler.command = /^(res(tart)?)$/i;
-handler.owner = true;
-
-export default handler;
+handler.help = ['اعادة_تشغيل']
+handler.tags = ['SASUKE', 'owner']
+handler.command = /^(اعادة_تشغيل|restart|sasuke_restart)$/i
+handler.owner = true
+export default handler
